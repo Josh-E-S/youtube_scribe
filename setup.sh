74eb0504d66c72b13ecd306181fbb532e7b96d5b
Josh Estrada <josh.est456@gmail.com>
2024-09-21 15:53:36 -0700

    Modified setup.sh file to known working with heroku

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"${STREAMLIT_EMAIL}\"\n\
+email = \"youremail@email.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = \$PORT\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
